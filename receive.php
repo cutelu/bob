92ee991521f12d087866f193e92c39bac57cb9d1
cutelu <47618522+cutelu@users.noreply.github.com>
2019-02-14 11:33:03 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,5 +1,5 @@
 <?php
-  $json_str = file_get_contents('php://input');
+  $json_str = file_get_contents("php://input");
   $json_obj = json_decode($json_str);
 
   $myfile = fopen("log.txt","w+") or die("Unable to open file!"); 
